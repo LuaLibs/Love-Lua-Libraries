df86c76ca92051f89ee83c1af4d7b4fa163da47b
Jeroen Broks <facebook@tbbs.nl>
2016-04-17 11:56:27 +0200

    Sun Apr 17 11:56:26 CEST 2016 Added tablemerge library

--- audio.lll/audio.lua
+++ audio.lll/audio.lua
@@ -17,6 +17,7 @@
      misrepresented as being the original software.
   3. This notice may not be removed or altered from any source distribution.
 ]]
+
 -- *import mkl_version
 
 mkl.version("Love Lua Libraries (LLL) - audio.lua","16.04.17")
---
+++ tablemerge.lll/tablemerge.lua
@@ -0,0 +1,31 @@
+--[[
+  tablemerge.lua
+  tablemerge
+  version: 16.04.17
+  Copyright (C) 2016 Jeroen P. Broks
+  This software is provided 'as-is', without any express or implied
+  warranty.  In no event will the authors be held liable for any damages
+  arising from the use of this software.
+  Permission is granted to anyone to use this software for any purpose,
+  including commercial applications, and to alter it and redistribute it
+  freely, subject to the following restrictions:
+  1. The origin of this software must not be misrepresented; you must not
+     claim that you wrote the original software. If you use this software
+     in a product, an acknowledgment in the product documentation would be
+     appreciated but is not required.
+  2. Altered source versions must be plainly marked as such, and must not be
+     misrepresented as being the original software.
+  3. This notice may not be removed or altered from any source distribution.
+]]
+
+-- *import append
+
+return function(table1,table2,kill) -- if kill is set numberic index will overwrite others in stead of just adding them. Otherwise this only happens to string keys.
+   for k,v in pairs(table2) do
+       if (not kill) and type(k)=='number' then
+          append(table1,v)
+       else   
+          table1[k] = v
+       end
+   end
+end          
