187d3a9f03264c11d61ca2d7543ff2e099a09098
Tricky1975 <facebook@tbbs.nl>
2017-08-15 21:47:58 +0200

    license info

--- lunamorica.lll/Gadget_Checkbox.lua
+++ lunamorica.lll/Gadget_Checkbox.lua
@@ -1,3 +1,13 @@
+--[[
+        Gadget_Checkbox.lua
+	(c) 2017 Jeroen Petrus Broks.
+	
+	This Source Code Form is subject to the terms of the 
+	Mozilla Public License, v. 2.0. If a copy of the MPL was not 
+	distributed with this file, You can obtain one at 
+	http://mozilla.org/MPL/2.0/.
+        Version: 17.08.15
+]]
 
 local function inside(g,x,y)
     return x>g.ax and x<g.ax+g.w and y>g.ay and y<g.ay+g.h
