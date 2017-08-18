644464524b9839ab1ba7a856dba465b0e1b5063f
Tricky1975 <facebook@tbbs.nl>
2017-08-18 20:03:39 +0200

    Empty base Kthura set

---
+++ Kthura.lll/kthura.lua
@@ -0,0 +1,18 @@
+--[[
+        kthura.lua
+	(c) 2017 Jeroen Petrus Broks.
+	
+	This Source Code Form is subject to the terms of the 
+	Mozilla Public License, v. 2.0. If a copy of the MPL was not 
+	distributed with this file, You can obtain one at 
+	http://mozilla.org/MPL/2.0/.
+        Version: 17.08.18
+]]
+-- *import ggfx2
+
+kthura = {}
+
+j_love_input("Core.lua")
+j_love_input("Draw.lua")
+
+return kthura
